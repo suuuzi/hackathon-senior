ef335cdb13ccdbbf54007694836acbb31813c641
suuuzi <tetestonaldo@gmail.com>
2016-08-19 21:25:28 -0300

    testes

--- aplicacao/src/app/app.js
+++ aplicacao/src/app/app.js
@@ -1,7 +1,6 @@
 (function() {
     'use strict';
     angular.module('templateApp', [
-
             'ui.router',
             'ui.bootstrap',
             'ngAnimate',
@@ -9,25 +8,33 @@
             'ngCookies',
             'highcharts-ng',
             'angularScreenfull'
-
-
         ])
         .controller('templateCtrl', ['$scope', '$http', function($scope, $http) {
             $scope.pagetitle = "Video Analytics";
             $scope.stockMonitor = "Monitorar reposição de gôndolas";
+						$scope.notificacoes = [
+							{"texto": "osdajiojdsad"},
+							{"texto": "3443"},
+							{"texto": "fdsfd"}
+						];
 
-						var url = "http://gondolaapi.mybluemix.net/PersonServlet";
+						var url = 'http://gondolaapi.mybluemix.net/PersonServlet';
+
 						$http({
-						    method: 'JSONP',
-						    url: url
-						}).
-						success(function(response) {
-							//console.log(response.nome);
-						    //return	status.nome;
-						}).
-						error(function(status) {
-						    //your code when fails
+						    method : 'GET',
+						    url : url,
+								headers: {
+									'Content-Type': 'application/json',
+									'Access-Control-Allow-Methods': '*',
+									'Access-Control-Allow-Origin': '*',
+									'Access-Control-Request-Headers': 'access-control-allow-methods, access-control-allow-origin'
+								}
+						}).then(function successCallback(response) {
+						    console.log(response.nome);
+						}, function errorCallback(response) {
+						    console.log(response.statusText);
 						});
+
 
         }])
         .config(configFunction);
@@ -38,12 +45,12 @@
     function configFunction($urlRouterProvider, $stateProvider, $httpProvider) {
         $urlRouterProvider.otherwise('/');
         $httpProvider.defaults.useXDomain = true;
-        $httpProvider.defaults.useXDomain = true;
         $httpProvider.defaults.withCredentials = true;
-        $httpProvider.defaults.headers.common["Accept"] = "application/json";
-        $httpProvider.defaults.headers.common["Content-Type"] = "application/json";
-				$httpProvider.defaults.headers.common["Access-Control-Allow-Origin"] = '*';
-        delete $httpProvider.defaults.headers.common['X-Requested-With'];
+	        $httpProvider.defaults.headers.common["Accept"] = "application/json";
+	        $httpProvider.defaults.headers.common["Content-Type"] = "application/json";
+					$httpProvider.defaults.headers.common["Access-Control-Allow-Origin"] = '*';
+					$httpProvider.defaults.headers.common["Access-Control-Allow-Methods"] = "*";
+	        delete $httpProvider.defaults.headers.common['X-Requested-With'];
         $stateProvider.state('sample', {
             url: "/",
             templateUrl: "app/modules/sampleModule/views/dashboard.html"
